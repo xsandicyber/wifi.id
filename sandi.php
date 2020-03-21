bb03d92b3550b33043e04964bf33a31d3af188c6
xsandicyber <55163084+xsandicyber@users.noreply.github.com>
2020-03-21 10:44:06 +0700

    Update sandi.php

--- sandi.php
+++ sandi.php
@@ -1,4 +1,5 @@
 <?php
+
 echo "Sandicyber\n";
 echo "Info : SPAM WHATSAPP MENGGUNAKAN API KEY BUKALAPAK.COM\n";
 echo "COPYRIGHT : Sandi.bc85\n\n";
@@ -55,4 +56,5 @@ Cookie: identity=".$rand1."; browser_id=".$rand2."; _ga=GA1.2.1024758930.1531960
 	curl_close($ch);
 	print $a.$nomer." [Sending]\n";
 }
-<?php
+
+?>
